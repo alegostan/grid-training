f63ae83f64d858f2877e38c75ce5a03c43e31b4c
Oleh Pozdniakov <oleh@test.io>
2023-11-29 18:26:28 +0200

    v0

---
+++ main.js
@@ -0,0 +1,6 @@
+function navTrigger() {
+    document.getElementsByClassName("nav")[0].classList.toggle("nav-visible");
+    document
+        .getElementsByClassName("grid-container")[0]
+        .classList.toggle("grid-container-nav");
+}
